5f5976d84af0df1832850ae8d1c88868817192ca
micheal-ndoh <michaelndoh9@gmail.com>
2025-06-29 19:55:38 +0100

    refactor: simplify handler function and remove Fibonacci computation

--- api/handler.rs
+++ api/handler.rs
@@ -1,40 +1,23 @@
-use num_bigint::{BigUint, ToBigUint};
 use serde_json::json;
+use std::collections::HashMap;
+use std::str::FromStr;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
+mod fib;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    run(fib_handler).await
+    run(handler).await
 }
 
-
-
-/// Computes the nth Fibonacci number using BigInt.
-pub fn fib(n: u32) -> BigUint {
-    let mut f0 = 0.to_biguint().unwrap();
-    let mut f1 = 1.to_biguint().unwrap();
-
-    if n == 0 {
-        f0
-    } else if n == 1 {
-        f1
-    } else {
-        for _ in 2..=n {
-            let f2 = &f0 + &f1;
-            f0 = f1;
-            f1 = f2;
-        }
-        f1
-    }
-}
-
-async fn fib_handler(_req: Request) -> Result<Response<Body>, Error> {
-
-    let result = fib(10);
-
+pub async fn handler(_req: Request) -> Result<Response<Body>, Error> {
     Ok(Response::builder()
-        .status(200)
+        .status(StatusCode::OK)
         .header("Content-Type", "application/json")
-        .body(Body::Text(format!(r#"{{"fib":"{}"}}"#, result)))
-  .unwrap())
+        .body(
+            json!({
+              "message": "你好，世界"
+            })
+            .to_string()
+            .into(),
+        )?)
 }
