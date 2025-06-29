a5f8cb697327efd1dbb8a20e8d1004fb673a3360
micheal-ndoh <michaelndoh9@gmail.com>
2025-06-29 17:07:36 +0100

    fix: correct BigInt usage and improve initialization in fibonacci function

--- api/fib.rs
+++ api/fib.rs
@@ -1,11 +1,12 @@
-use bigInt::BigInt;
+use num_bigint::BigInt;
+use num_traits::FromPrimitive;
 use serde_json::json;
 use vercel_runtime::{run, Body, Error, Request, Response, StatusCode};
 
 pub fn fibonacci(n: i64) -> BigInt {
     match n {
-        0 => BigInt::from(0),
-        1 => BigInt::from(1),
+        0 => BigInt::from_i64(0).unwrap(),
+        1 => BigInt::from_i64(1).unwrap(),
         n => fibonacci(n - 1) + fibonacci(n - 2),
     }
 }
